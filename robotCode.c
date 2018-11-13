ddf5d071d5380dd994f33a72935f9534f37513e5
Charlie Fisher <charliefisherhome@gmail.com>
2018-11-13 14:08:44 -0500

    Fixed error with final point of line

--- robotCode.c
+++ robotCode.c
@@ -4,7 +4,7 @@ const int X_SPEED=20; //mm/s
 const int AXIS_STEP = 2; //mm
 const int MAX_X = 224; //mm
 const int MAX_Y = 224; //mm
-const int POINT_DISTANCE = MAX_X/POINTS_PER_LINE;
+const int POINT_DISTANCE = MAX_X/POINTS_PER_LINE; // mm - distance between adjacent points
 const tMotor X_AXIS = motorD;
 const tMotor Y_AXIS = motorA;
 const tMotor Z_AXIS = motorC;
@@ -16,9 +16,15 @@ const tSensors SCANNER_SENSOR = S4;
 float calculateRPM();
 
 int getDistanceToNextPoint(bool* points, int currentPoint) {
-	int nextPoint = currentPoint+1;
-	for (nextPoint = currentPoint; points[nextPoint] == 0; nextPoint++);
-	return nextPoint-currentPoint;
+	int nextPoint = 0;
+	// traverse the array to find the next point we plot - by default we say this is the next index in the array
+	for (nextPoint = currentPoint+1; nextPoint < POINTS_PER_LINE && points[nextPoint] == 0; nextPoint++);
+
+	// check that we actually found a point and return it, if not, return -1
+	// this check is necessary for the case where our current point is the last point
+	if (nextPoint < POINTS_PER_LINE)
+		return nextPoint-currentPoint;
+	return -1;
 }
 
 void zeroAllAxis();
@@ -26,13 +32,23 @@ void zeroAllAxis();
 void zeroAxis();
 
 void adjustPenSpeed(bool* points, int* speeds) {
-	int distanceToNextPoint = 0;
+	// distanceToNextPoint is in mm
+	// speed is in degrees per second
+	int distanceToNextPoint = 0, speed = 0;
 
 	for (int currentPoint = 0; currentPoint < POINTS_PER_LINE; currentPoint += distanceToNextPoint) {
 		distanceToNextPoint = getDistanceToNextPoint(points, currentPoint);
-		int timeToNextPoint = distanceToNextPoint*POINT_DISTANCE / X_SPEED;
-		// speed is in degrees per second
-		int speed = 360 / timeToNextPoint;
+
+		if (distanceToNextPoint != -1)
+		{
+			int timeToNextPoint = distanceToNextPoint*POINT_DISTANCE / X_SPEED;
+			speed = 360 / timeToNextPoint;
+		}
+		else
+		{
+			speed = 0;
+			distanceToNextPoint = POINTS_PER_LINE-currentPoint;
+		}
 
 		for (int pointToSet = currentPoint; pointToSet < currentPoint+distanceToNextPoint; pointToSet++)
 			speeds[pointToSet] = speed;
@@ -45,8 +61,8 @@ void readNextLine();
 
 void stopMovement()
 {
-	while(SensorValue[Z_LIMIT_SWITCH] == 0)
-	{}
+	// wait until we reach the place in the array
+	while();
 	motor[X_AXIS] = motor[Y_AXIS] = motor[Z_AXIS] = 0;
 }
 
