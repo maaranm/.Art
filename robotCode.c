62591374791c01e115d15fde1657860f8f061705
Aidan Baksh <aidanbaksh@gmail.com>
2018-11-13 13:22:40 -0500

    ZeroAxis() written, not tested

--- robotCode.c
+++ robotCode.c
@@ -3,28 +3,47 @@ const int AXIS_STEP = 2; //mm
 const int MAX_X = 224; //mm
 const int MAX_Y = 224; //mm
 const tMotor X_AXIS = motorD;
-const int tMotor Y_AXIS = motorA;
-const int tMotor Z_AXIS = motorC;
-const int tSensors X_LIMIT_SWITCH = S2;
-const int tSensors Y_LIMIT_SWITCH = S1;
-const int tSensors Z_LIMIT_SWITCH = S3;
-const int tSensors SCANNER_SENSOR = S4;
+const tMotor Y_AXIS = motorA;
+const tMotor Z_AXIS = motorC;
+const tSensors X_LIMIT_SWITCH = S2;
+const tSensors Y_LIMIT_SWITCH = S1;
+const tSensors Z_LIMIT_SWITCH = S3;
+const tSensors SCANNER_SENSOR = S4;
 
 float calculateRPM();
 
 int timeToNextPoint();
 
 void zeroAllAxis(){
-	
+	zeroAxis
 }
 
-void zeroAxis();
+void zeroAxis(int axis){ //0 = x , 1 = y , 2 = z
+	if(axis == 0){
+		motor[X_AXIS] = -25;
+		while (!SensorValue[X_LIMIT_SWITCH]);
+		motor[X_AXIS] = 0;
+		nMotorEncoder[X_AXIS] = 0;
+	} else if (axis == 1){
+		motor[Y_AXIS] = 100;
+		while (!SensorValue[Y_LIMIT_SWITCH]);
+		motor[Y_AXIS] = 0;
+		nMotorEncoder[Y_AXIS] = 0;
+	} else {
+		motor[Z_AXIS] = 50;
+		while (!SensorValue[Z_LIMIT_SWITCH]);
+		motor[Z_AXIS] = 0;
+		nMotorEncoder[Z_AXIS] = 0;
+	}
+}
 
 void adjustPenSpeed();
 
 int calculatePID();
 
-void readNextLine();
+void readNextLine(){
+	
+}
 
 void stopMovement();
 
