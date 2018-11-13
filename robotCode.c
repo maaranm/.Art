0ff087fc5e25f166b81c855f86001668e099154d
Charlie Fisher <charliefisherhome@gmail.com>
2018-11-13 13:49:43 -0500

    Implemented all functions

--- robotCode.c
+++ robotCode.c
@@ -1,33 +1,54 @@
+
+const int POINTS_PER_LINE = 80;
 const int X_SPEED=20; //mm/s
 const int AXIS_STEP = 2; //mm
 const int MAX_X = 224; //mm
 const int MAX_Y = 224; //mm
+const int POINT_DISTANCE = MAX_X/POINTS_PER_LINE;
 const tMotor X_AXIS = motorD;
-const int tMotor Y_AXIS = motorA;
-const int tMotor Z_AXIS = motorC;
-const int tSensors X_LIMIT_SWITCH = S2;
-const int tSensors Y_LIMIT_SWITCH = S1;
-const int tSensors Z_LIMIT_SWITCH = S3;
-const int tSensors SCANNER_SENSOR = S4;
+const tMotor Y_AXIS = motorA;
+const tMotor Z_AXIS = motorC;
+const tSensors X_LIMIT_SWITCH = S2;
+const tSensors Y_LIMIT_SWITCH = S1;
+const tSensors Z_LIMIT_SWITCH = S3;
+const tSensors SCANNER_SENSOR = S4;
 
 float calculateRPM();
 
-int timeToNextPoint()
-{
-
+int getDistanceToNextPoint(bool* points, int currentPoint) {
+	int nextPoint = currentPoint+1;
+	for (nextPoint = currentPoint; points[nextPoint] == 0; nextPoint++);
+	return nextPoint-currentPoint;
 }
 
 void zeroAllAxis();
 
 void zeroAxis();
 
-void adjustPenSpeed();
+void adjustPenSpeed(bool* points) {
+	int distanceToNextPoint = 0;
+
+	for (int currentPoint = 0; currentPoint < POINTS_PER_LINE; currentPoint += distanceToNextPoint) {
+		distanceToNextPoint = getDistanceToNextPoint(points, currentPoint);
+		int timeToNextPoint = distanceToNextPoint*POINT_DISTANCE / X_SPEED;
+		// speed is in degrees per second
+		int speed = 360 / timeToNextPoint;
+
+		for (int pointToSet = currentPoint; pointToSet < currentPoint+distanceToNextPoint; pointToSet++)
+			points[pointToSet] = speed;
+	}
+}
 
 int calculatePID();
 
 void readNextLine();
 
-void stopMovement();
+void stopMovement()
+{
+	while(SensorValue[Z_LIMIT_SWITCH] == 0)
+	{}
+	motor[X_AXIS] = motor[Y_AXIS] = motor[Z_AXIS] = 0;
+}
 
 void moveYAxis();
 
