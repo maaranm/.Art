7116f0f47c37bc763402b0d638524ae305f3869d
Maaran Murugathas <tmurugat@uwaterloo.ca>
2018-11-13 13:47:45 -0500

    Added basic PID function

--- robotCode.c
+++ robotCode.c
@@ -3,14 +3,17 @@ const int AXIS_STEP = 2; //mm
 const int MAX_X = 224; //mm
 const int MAX_Y = 224; //mm
 const tMotor X_AXIS = motorD;
-const int tMotor Y_AXIS = motorA;
-const int tMotor Z_AXIS = motorC;
-const int tSensors X_LIMIT_SWITCH = S2;
-const int tSensors Y_LIMIT_SWITCH = S1;
-const int tSensors Z_LIMIT_SWITCH = S3;
-const int tSensors SCANNER_SENSOR = S4;
+const tMotor Y_AXIS = motorA;
+const tMotor Z_AXIS = motorC;
+const tSensors X_LIMIT_SWITCH = S2;
+const tSensors Y_LIMIT_SWITCH = S1;
+const tSensors Z_LIMIT_SWITCH = S3;
+const tSensors SCANNER_SENSOR = S4;
 
-float calculateRPM();
+float lastError = 0;
+
+float calculateRPM(){
+}
 
 int timeToNextPoint();
 
@@ -20,7 +23,12 @@ void zeroAxis();
 
 void adjustPenSpeed();
 
-int calculatePID();
+int calculatePID(float target, float curVal, float kp, float kd){
+	float error = target-curVal;
+	int outputPow = error*kp + lastError*kd;
+	lastError = error;
+	return outputPow;
+}
 
 void readNextLine();
 
