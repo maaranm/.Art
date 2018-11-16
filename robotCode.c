6ff079e2f1069aa734283bc84039c05450c8e499
Emily Steiner <easteine@edu.uwaterloo.ca>
2018-11-16 17:49:20 -0500

    Function moveYAxis, moveXAxis and Pause, added pause global variable
    move axis distance in mm

--- robotCode.c
+++ robotCode.c
@@ -9,6 +9,7 @@ const int tSensors X_LIMIT_SWITCH = S2;
 const int tSensors Y_LIMIT_SWITCH = S1;
 const int tSensors Z_LIMIT_SWITCH = S3;
 const int tSensors SCANNER_SENSOR = S4;
+bool isPaused = true;
 
 float calculateRPM();
 
@@ -26,11 +27,53 @@ void readNextLine();
 
 void stopMovement();
 
-void moveYAxis();
+void moveYAxis(int distance)
+{
+	const float WHEEL_DIA= 4.25;
+	const float ENC_LIMIT= (distance/100.0)*180*25/(PI*WHEEL_DIA);
+	// geared down 25 to 1
+	int motorSpeed = 20;
+	int hault =0;
+
+	if (distance <0)
+		motorSpeed *= -1;
+
+	nMotorEncoder[Y_AXIS]=0;
+	motor[Y_AXIS]=motorSpeed;
+
+	while (nMotorEncoder[Y_AXIS] < ENC_LIMIT)
+	{}
+
+	motor[Y_AXIS]= hault;
+}
+
+void moveXAxisDist (int distance)
+{
+	const float PINION_CIRC = 25.44;
+	const float ENC_LIMIT = (distance/100.0)*180/PINION_CIRC;
+
+	int motorSpeed = 90;
+	int hault =0;
+
+	if (distance <0)
+		motorSpeed *= -1;
+
+	nMotorEncoder[X_AXIS]=0;
+	motor[X_AXIS]=motorSpeed;
+
+	while (nMotorEncoder[X_AXIS] < ENC_LIMIT)
+	{}
+
+	motor[X_AXIS]= hault;
+}
+
+
 
 void pause()
 {
-	
+	if (isPaused == false)
+		stopMovement;
+	isPaused = !isPaused;
 }
 
 void scan();
